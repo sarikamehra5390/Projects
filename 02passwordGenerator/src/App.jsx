6abeeb4b8dbac08c34df4ce696add1de61c70d4e
Sarika Mehra <mehrasarika574@gmail.com>
2026-05-27 23:18:36 +0530

    new project is added

---
+++ 02passwordGenerator/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useCallback } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [Password, setPassword] = useState("")
+
+  //useCallBack : is a react hook that lets you cache a function defination between re-renders
+  const PasswordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrtuvwxyz"
+    
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*-_+=[](){}~`"
+
+    for (let i = 1; i <= length; i++){
+       let char = Math.floor(Math.random() * str.length + 1)
+
+       pass += str.charAt(char)
+     
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  return (
+    <>
+      <div className="w-full text-center max-w-md mx-auto shadow-md rounded-lg px-4 my-10 py-4 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          type="text"
+          value={Password}
+          className="outline-none w-full py-1 px-3 bg-white text-gray-500"
+          placeholder="Password"
+          readOnly
+          />
+
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="range" 
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value)
+            }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+              <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() =>{
+                // it is used to flip the values like if there is true then it will be false and vice-versa
+                   setNumberAllowed((prev) => !prev);
+              }}
+              />
+              <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+              <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() =>{
+                // it is used to flip the values like if there is true then it will be false and vice-versa
+                   setCharAllowed((prev) => !prev);
+              }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
